4ae4ff66daa19d8e3fec9e165b405906053030ba
Rubén Soria <rusok.software@gmail.com>
2025-09-19 20:26:17 -0400

    Agregando endpoints

---
+++ src/main/java/com/wintux/ejemplodevops/EjemploDevOps1Application.java
@@ -0,0 +1,13 @@
+package com.wintux.ejemplodevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploDevOps1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploDevOps1Application.class, args);
+	}
+
+}
