a26934cf875ecba6ff5cec7483a9994557296290
Rubén Soria <rusok.software@gmail.com>
2025-09-25 20:31:09 -0400

    Para manejo de variables de entorno

--- src/main/java/com/wintux/ejemplodevops/Controllers/PruebaController.java
+++ src/main/java/com/wintux/ejemplodevops/Controllers/PruebaController.java
@@ -1,5 +1,6 @@
 package com.wintux.ejemplodevops.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1") // http://localhost:8080/api/v1
 public class PruebaController {
+    @Value("${password.system.connection}")
+    private String dbPassword;
     @GetMapping("/prueba") // http://localhost:8080/api/v1/prueba [GET]
     public String unEndpoint(){
         return "[GET] Completado";
+    }
+    @GetMapping("/secret")
+    public String getSecreto(){
+        return "El valor de DB_PASSWORD es" + dbPassword;
     }
 }
